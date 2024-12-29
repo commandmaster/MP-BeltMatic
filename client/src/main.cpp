f9388c8869966bceef911a3802806e5975e41307
bennettf <friesenbennett@gmail.com>
2024-12-29 15:46:16 -0600

    switched to cmake

---
+++ client/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <enet/enet.h>
+
+int main() {
+    std::cout << "Client Application Started" << std::endl;
+
+
+
+    return 0;
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Server Application Started" << std::endl;
+    return 0;
+}
